e77dd5ffc583ec683671f27ff4fb62a91074f571
ptsoo <46866533+xotn1270@users.noreply.github.com>
2023-12-18 01:43:23 +0900

    Update example.cpp
    예제 수정
    - jthread 사용으로 push 전부 끝내고, pop 하고 있는 부적절한 예제 수정

--- example.cpp
+++ example.cpp
@@ -1,6 +1,5 @@
 #include "MPSCQueue.h"
 
-#include <conio.h>
 #include <thread>
 
 TSUtil::CMPSCQueue<size_t> queue;
@@ -8,54 +7,50 @@ TSUtil::CMPSCQueue<size_t> queue;
 int main()
 {
 	// example
-	const size_t threadCount = 5;
+	const size_t threadCount = 3;
 	const size_t pushCount = 10;
 
 	const size_t maxPopCount = threadCount * pushCount;
 
-	// producer
-	{
-		std::vector<std::jthread> lstPublishThread;
-		lstPublishThread.reserve(threadCount);
-
-		for (size_t i = 0; i < threadCount; ++i)
-		{
-			lstPublishThread.emplace_back(
-				[tid = i]()
-				{
-					const size_t offset = tid * pushCount;
-					for (size_t i = 0; i < pushCount; ++i)
-					{
-						printf("push: %zu \n", offset + i);
-						queue.push(offset + i);
-					}
-				}
-			);
-		}
-	}
-
 	// consumer
+	std::jthread consumeThread
 	{
-		std::jthread consumeThread
+		[]()
 		{
-			[]()
+			size_t count = 0;
+			while (count < maxPopCount)
 			{
-				size_t count = 0;
-				while (count < maxPopCount)
-				{
-					if (queue.swap() == false)
-						continue;
+				if (queue.swap() == false)
+					continue;
 
-					auto readQueue = queue.getReadQueue();
-					while (readQueue->empty() == false)
-					{
-						printf("pop: %zu \n", readQueue->front());
-						readQueue->pop();
-						++count;
-					}
+				auto readQueue = queue.getReadQueue();
+				while (readQueue->empty() == false)
+				{
+					printf("pop: %zu \n", readQueue->front());
+					readQueue->pop();
+					++count;
 				}
 			}
-		};
+		}
+	};
+
+	// producer
+	std::vector<std::jthread> lstPublishThread;
+	lstPublishThread.reserve(threadCount);
+
+	for (size_t i = 0; i < threadCount; ++i)
+	{
+		lstPublishThread.emplace_back(
+			[tid = i]()
+			{
+				const size_t offset = tid * pushCount;
+				for (size_t i = 0; i < pushCount; ++i)
+				{
+					printf("push: %zu \n", offset + i);
+					queue.push(offset + i);
+				}
+			}
+		);
 	}
 
 	return 0;
